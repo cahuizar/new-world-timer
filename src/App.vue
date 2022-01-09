9e7cebb960c4fd8d731b1e6f49f2b4c6c7eed38b
Carlos Huizar <hire@carloshuizar.com>
2022-01-09 09:46:20 -0500

    Resolve bugs

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <v-app>
     <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
-       <h2>New World</h2>
+        <h2>New World</h2>
       </div>
     </v-app-bar>
 
--- src/main.js
+++ src/main.js
@@ -3,15 +3,13 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
-import VueCountdownTimer from 'vuejs-countdown-timer';
-import { VueMaskDirective } from 'v-mask'
-
+import VueCountdownTimer from "vuejs-countdown-timer";
+import { VueMaskDirective } from "v-mask";
 
 Vue.use(VueCountdownTimer);
 Vue.config.productionTip = false;
 
-Vue.directive('mask', VueMaskDirective);
-
+Vue.directive("mask", VueMaskDirective);
 
 new Vue({
   router,
