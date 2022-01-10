5b68f68d6d1736e962a99f5ac26b3ab14bc4db94
Carlos Huizar <hire@carloshuizar.com>
2022-01-09 21:24:55 -0500

    Add theme toggler functionality

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,14 @@
       <div class="d-flex align-center">
         <h2>New World</h2>
       </div>
+      <v-spacer></v-spacer>
+      <v-btn class="mr-4" icon v-if="!$vuetify.theme.dark" @click="handledarkmode">
+        <v-icon color="white">mdi-lightbulb</v-icon>
+      </v-btn>
+      <v-btn class="mr-4" icon v-if="$vuetify.theme.dark" @click="handledarkmode">
+        <v-icon color="yellow darken-3">mdi-lightbulb-outline</v-icon>
+      </v-btn>
     </v-app-bar>
-
     <v-main>
       <router-view />
     </v-main>
@@ -15,9 +21,38 @@
 <script>
 export default {
   name: "App",
-
-  data: () => ({
-    //
-  }),
+  data () {
+    return {
+      darkmode: false
+    }
+  },
+  created () {
+    if (localStorage.getItem('DarkMode')) {
+      const cookieValue = localStorage.getItem('DarkMode') === 'true';
+      this.darkmode = cookieValue;
+      this.$vuetify.theme.dark = cookieValue
+    } else {
+      this.handledarkmode();
+    }
+  },
+  methods: {
+    handledarkmode () {
+      if (this.darkmode === true) {
+        this.$vuetify.theme.dark = false
+        localStorage.setItem('DarkMode', false);
+        this.darkmode = false
+      } else if (this.darkmode === false) {
+        this.$vuetify.theme.dark = true
+        localStorage.setItem('DarkMode', true)
+        this.darkmode = true;
+      }
+    }
+  }
 };
 </script>
+<style lang="scss">
+.switch-center {
+  display: flex;
+  justify-content: center;
+}
+</style>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,22 @@
+import '@mdi/font/css/materialdesignicons.css'
 import Vue from "vue";
 import Vuetify from "vuetify/lib/framework";
+import colors from 'vuetify/lib/util/colors';
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  icons: {
+    iconfont: 'mdi',
+  },
+  theme: {
+    dark: false,
+    themes: {
+      dark: {
+        primary: colors.blueGrey.darken2,
+        secondary: colors.blueGrey.lighten2,
+        accent: colors.blueGrey.darken3,
+      },
+    },
+  },
+})
